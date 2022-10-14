8272ff287d166299445b14cf4c3443957b850a76
Felix <bosirefelix97@gmail.com>
2022-10-14 18:12:38 +0300

    updates

--- new/office/models.py
+++ new/office/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class patient(models.Model):
-    firt_name = models.CharField(max_length=30)
-    second_name = models.CharField(max_length=30)
-    age= models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age= models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(120)])
+    rate=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(300)])
+
+    def __str__(self):
+        return f"{self.last_name},{self.first_name} is {self.age} years old and heart rate of {self.rate}b/s"
